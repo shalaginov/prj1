14ceadf4989c46558dc736b96c0e1736c4a82a62
shalaginov <denis.shalaginov@gmail.com>
2017-11-23 16:49:31 +0500

    Update test.java

--- src/prj1/test.java
+++ src/prj1/test.java
@@ -11,6 +11,7 @@ public class test {
 	public void testMaxC() {
 		int result = Program.maxC(1200, 800, 1400, 750,50, 150, 250, 0.5 ); //тестовые параметры
 		assertEquals(result, 600); //ожидаемый результат
+		System.out.println ("Значение поля равно "+result);
 	}
 
 }
